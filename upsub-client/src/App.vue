8d7bb7233eb60fdbe1c6eb2a49d173d3188ccb79
marax27 <kacpert314@gmail.com>
2022-06-05 18:50:00 +0200

    Create MainDashboard

--- upsub-client/src/App.vue
+++ upsub-client/src/App.vue
@@ -1,86 +1,53 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import MainDashboard from "./components/MainDashboard.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="navbar bg-primary">
+    <section class="navbar-section">
+      <a href="/">
+        <img alt="Vue logo" class="logo" src="./assets/logo.svg" height="36" />
+      </a>
+      <span class="navbar-brand ml-2 mr-2">Welcome to Upsub</span>
+    </section>
   </header>
 
   <main>
-    <h1>WIP</h1>
+    <MainDashboard msg="You did it!" />
   </main>
+
+  <footer class="footer bg-dark">
+    <span class="ml-2">&copy; 2020 Upsub. All rights reserved.</span>
+  </footer>
 </template>
 
 <style>
 @import "./assets/base.css";
 
 #app {
-  max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
 
   font-weight: normal;
 }
 
-header {
+header.navbar {
+  padding: 0.25rem;
   line-height: 1.5;
+  border-bottom: solid 1px var(--color-border);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+main {
+  margin-bottom: calc(0.5rem + var(--footer-height));
 }
 
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
+.footer {
+  position: fixed;
+  height: var(--footer-height);
+  left: 0;
+  bottom: 0;
+  right: 0;
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  display: flex;
+  align-items: center;
 }
 </style>
---
+++ upsub-client/src/components/MainDashboard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="container">
+    <div class="columns">
+      <div
+        v-for="i in [1, 2, 3, 4, 5, 6]"
+        :key="i"
+        class="column col-6 col-lg-12"
+      >
+        <div class="card">
+          <div class="card-header">
+            <div class="card-title h5">Title</div>
+            <div class="card-subtitle text-gray">Subtitle</div>
+          </div>
+          <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            ducimus minus in neque, velit earum ipsum totam error debitis
+            aliquam reiciendis corrupti, cumque vero voluptas quia magni dolorem
+            blanditiis! Saepe.
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-primary">...</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
